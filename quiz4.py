9f5ec37f838b9e1f3f7e7e059c21a9cea1b7c82c
Steven Solis <steven17solis@gmail.com>
2020-01-27 11:29:50 -0600

    Add files via upload

---
+++ quiz4.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 19:31:25 2019
+
+@author: Steven Solis
+"""
+
+def subtract(x, y):
+   return x - y
+
+def add(x, y):
+   return x + y
+
+def divide(x, y):
+   return x / y
+
+def multiply(x, y):
+   return x * y
+
+print("Select operation")
+print("1- Subtract")
+print("2- Add")
+print("3- Divide")
+print("4- Multiply")
+ 
+choice = input("Enter choice(1/2/3/4):")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+   
+elif choice == '2':
+   print(num1,"+",num2,"=", add(num1,num2))
+   
+elif choice == '3':
+   print(num1,"/",num2,"=", divide(num1,num2))
+   
+elif choice == '4':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+   
+else:
+   print("Invalid input")
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
